71089dc5b5f388ede05b35ba08b06ea84a710cb6
Hiroshi Hatake <hatake@clear-code.com>
2016-09-21 15:46:02 +0900

    in_ping_message: Drop ensuring backward compatibility code

--- lib/fluent/plugin/in_ping_message.rb
+++ lib/fluent/plugin/in_ping_message.rb
@@ -6,21 +6,11 @@ class Fluent::Plugin::PingMessageInput < Fluent::Plugin::Input
 
   helpers :timer
 
-  # Define `log` method for v0.10.42 or earlier
-  unless method_defined?(:log)
-    define_method("log") { $log }
-  end
-
   include Fluent::Mixin::ConfigPlaceholders
 
   config_param :tag, :string, :default => 'ping'
   config_param :interval, :integer, :default => 60
   config_param :data, :string, :default => `hostname`.chomp
-
-  # Define `router` method of v0.12 to support v0.10.57 or earlier
-  unless method_defined?(:router)
-    define_method("router") { Fluent::Engine }
-  end
 
   def start
     super
